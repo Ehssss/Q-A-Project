edab1a060367e71cb95553973b9ccd5766f4713e
Ehsaan Alam <ehsaanalam0809@gmail.com>
2023-03-24 16:27:53 +0000

    idrk

--- config/config.exs
+++ config/config.exs
@@ -62,3 +62,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+// tailwind.config.js
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ],
+}
