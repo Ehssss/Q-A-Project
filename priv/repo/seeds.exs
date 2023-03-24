290b8e806dce066514731df92c822754be46a5bd
Daniel-multiverse <daniel.oyewole@multiverse.io>
2023-03-24 12:17:25 +0000

    more seed stuff

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,17 +5,32 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     Qna.Repo.insert!(%Qna.SomeSchema{})
-Qna.Repo.insert(
-  Qna.Users,
-  [
-    %{username: "Alice", email: "alice@example.com", password: "password"},
-    %{username: "Bob", email: "bob@example.com", password: "password"},
-    %{username: "Charlie", email: "charlie@example.com", password: "password"}
-  ],
-  on_conflict: :replace_all
-)
+users = [
+  %{
+    username: "Alice",
+    email: "alice@example.com",
+    password: "password",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  },
+  %{
+    username: "Bob",
+    email: "bob@example.com",
+    password: "password",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  },
+  %{
+    username: "Charlie",
+    email: "charlie@example.com",
+    password: "password",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  }
+]
 
+#     Qna.Repo.insert!(%Qna.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Qna.Repo.insert_all(Qna.Users, users)
