a0d4fcaa247ed40118eba156b867a7d2f62de3c9
Ehsaan Alam <ehsaanalam0809@gmail.com>
2023-03-27 15:08:07 +0100

    Question table and users table able to retrieve and use info

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -29,8 +29,40 @@ users = [
   }
 ]
 
+questions = [
+  %{
+    body: "what came first, the chicken or the egg?",
+    upvotes: 10,
+    userid: 1,
+    username: "First",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  },
+
+  %{
+    body: "Why did the chicken cross the road?",
+    upvotes: 80,
+    userid: 2,
+    username: "Second",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  },
+
+  %{
+    body: "What is Obama's last name?",
+    upvotes: 800,
+    userid: 3,
+    username: "3th",
+    inserted_at: NaiveDateTime.truncate(DateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(DateTime.utc_now(), :second)
+  }
+
+
+]
+
 #     Qna.Repo.insert!(%Qna.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 Qna.Repo.insert_all(Qna.Users, users)
+Qna.Repo.insert_all(Qna.Questions, questions)
